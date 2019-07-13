eec560a06614f357799ef85a5f91a1076f835a22
andreizab <soslowpokeslowpoke@gmail.com>
2019-07-13 17:38:02 +0300

    Перенос коммента не работает + чек на кол-во символов

--- src/asm/parse_asm.c
+++ src/asm/parse_asm.c
@@ -6,7 +6,7 @@
 /*   By: rhealitt <rhealitt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/08 15:14:10 by rhealitt          #+#    #+#             */
-/*   Updated: 2019/07/12 19:14:48 by rhealitt         ###   ########.fr       */
+/*   Updated: 2019/07/13 17:20:50 by rhealitt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +25,37 @@ int			ft_strncmp(char *s1, char *s2, unsigned int n)
 	return (s1[i] - s2[i]);
 }
 
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char			*dest;
+	unsigned int	i;
+
+	i = 0;
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	dest = (char*)malloc(sizeof(*dest) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (dest == NULL)
+		return (NULL);
+	while (*s1 != '\0')
+		dest[i++] = *s1++;
+	while (*s2 != '\0')
+		dest[i++] = *s2++;
+	dest[i] = '\0';
+	return (dest);
+}
+
 int			ft_free_l(char *line)
 {
 	if (line)
 		free(line);
 	return (1);
+}
+
+void	ft_free_data(t_data *data)
+{
+	free(data->name);
+	free(data->comment);
+	free(data);
 }
 
 t_data	*ft_create(int fd)
@@ -46,26 +72,45 @@ char		*ft_get_name_or_comment(char *src, t_data *data)
 {
 	int i;
 	int j;
-	int quotes;
 	char *dst;
 
 	i = 0;
 	j = 0;
-	quotes = 0;
+	data->quotes = 0;
 	while (src[i] && src[i] != '"')
 		src[i] =='\'' ? ft_error("WRONG_QUOTES", data) : i++;
 	dst = (char *)ft_memalloc(sizeof(ft_strlen(src + i)));
 	if (src[++i - 1] == '"')
-		quotes++;
+		data->quotes++;
 	while (src[i] && src[i] != '"')
 		src[i] =='\'' ? ft_error("WRONG_QUOTES", data) : (dst[j++] = src[i++]);
 	while (src[i])
 		if (src[i++] == '"' )
-			quotes++;
-	if (quotes > 2)
+			data->quotes++;
+	if (data->quotes > 2)
 		ft_error("TOO_MANY_QUOTES", data);
-	if (quotes < 2)
-		ft_error("NO_ENOUGH_QUOTES", data);
+	//if (data->quotes < 2)
+//		ft_error("NO_ENOUGH_QUOTES", data);
+	dst[j] = '\0';
+	return (dst);
+}
+
+char		*ft_add_text(char *src, t_data *data)
+{
+	int i;
+	int j;
+	char *dst;
+
+	i = 0;
+	j = 0;
+	dst = (char *)ft_memalloc(sizeof(ft_strlen(src + i)));
+	while (src[i] && src[i] != '"')
+		src[i] =='\'' ? ft_error("WRONG_QUOTES", data) : (dst[j++] = src[i++]);
+	while (src[i])
+		if (src[i++] == '"' )
+			data->quotes++;
+	if (data->quotes > 2)
+		ft_error("TOO_MANY_QUOTES", data);
 	dst[j] = '\0';
 	return (dst);
 }
@@ -77,26 +122,36 @@ void		ft_row_is_data(t_data *data, char *str, int i)
 		while (str[i] == ' ' || str[i] == '\t')
 			i++;
 		if (str[i] == COMMENT_CHAR || str[i] == ALT_COMMENT_CHAR)
-			return ;
+			return;
 		if (str[i] == NAME_CMD_STRING[0] && !ft_strncmp(str + i, NAME_CMD_STRING, ft_strlen(NAME_CMD_STRING)))
 		{
 			if (data->name)
-				ft_error("DOUBLE_NAME", data);
+				ft_error("NAME_ALREADY_EXIST", data);
 			else
-				data->name = ft_get_name_or_comment(str + i, data);
+			{
+				if (data->quotes == 1)
+					data->name = ft_strjoin(data->name, ft_add_text(str, data));
+				else
+					data->name = ft_get_name_or_comment(str + i, data);
+			}
 		}
-		if (str[i] == COMMENT_CMD_STRING[0] && !ft_strncmp(str + i, COMMENT_CMD_STRING, ft_strlen(COMMENT_CMD_STRING)))
+		else if (str[i] == COMMENT_CMD_STRING[0] &&
+				   !ft_strncmp(str + i, COMMENT_CMD_STRING, ft_strlen(COMMENT_CMD_STRING)))
 		{
 			if (data->comment)
-				ft_error("DOUBLE_COMMENT", data);
+				ft_error("COMMENT_ALREADY_EXIST", data);
 			else
-				data->comment = ft_get_name_or_comment(str + i, data);
+			{
+				if (data->quotes == 1)
+					data->comment = ft_strjoin(data->comment, ft_add_text(str, data));
+				else
+					data->comment = ft_get_name_or_comment(str + i, data);
+			}
 		}
 	}
-
 }
 
-void		ft_row_is_code (t_data *data, char *str,int i)
+void		ft_row_is_code (t_data *data, char *str, int i)
 {
 	while (str[++i])
 	{
@@ -123,6 +178,7 @@ void		ft_read(t_data *data)
 	line = NULL;
 	while (ft_free_l(line) && (err = get_next_line(data->fd, &line)) > 0)
 	{
+		data->count_char += ft_strlen(line);
 		data->num_current_row++;
 		i = 0;
 		while (line[i] == ' ' || line[i] == '\t')
@@ -146,7 +202,5 @@ void	ft_asm(char *str)
 		ft_error("FILE_NOT_FOUND", NULL);
 	data = ft_create(fd);
 	ft_read(data);
-	free(data->name);
-	free(data->comment);
-	free(data);
+	ft_free_data(data);
 }
