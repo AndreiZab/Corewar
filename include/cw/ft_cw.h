1b3081b1ff4c516beb0ed0aa373b8ccf86032ca2
PofigistVip <pruskalev@gmail.com>
2019-07-14 10:35:24 +0300

    first stages

--- include/cw/ft_cw.h
+++ include/cw/ft_cw.h
@@ -1,23 +1,41 @@
 #ifndef FT_CW_H
 # define FT_CW_H
+# include <fcntl.h>
 # include "libft.h"
 # include "op.h"
+# define FT_OK 0
+# define FT_NO_ARGS -1
+# define FT_N_FLAG -2
+# define FT_READ_FILE -3
+# define FT_EXE_MAGIC -4
+# define FT_4_NULL -5
+# define FT_MAX_SIZE -6
+# define FT_EXE_MAX -7
+
+typedef struct	s_player
+{
+	unsigned char	id;
+	char			*name;
+	char			*comment;
+
+	int				exe_size;
+	char			*exe;
+
+}				t_player;
 
 typedef struct	s_corewar
 {
+	int				dump;
+
 	unsigned char	map[MEM_SIZE];
 
-	t_player		players[MAX_PLAYERS];
+	struct s_player	players[MAX_PLAYERS];
 	int				players_count;
 
 
 }				t_corewar;
 
-typedef struct	s_player
-{
-	char	id;
 
-}				t_player;
 
 typedef struct	s_carriage
 {
--- src/cw/main.c
+++ src/cw/main.c
@@ -1,8 +1,33 @@
 #include "ft_cw.h"
 
+int		ft_prepare_field(t_corewar *cw)
+{
+	int mem_part;
+	int i;
+
+	ft_bzero(cw->map, MEM_SIZE);
+	mem_part = MEM_SIZE / cw->players_count;
+	i = -1;
+	while (++i < cw->players_count)
+	{
+		ft_memcpy(cw->map[i * mem_part], cw->players[i].exe,
+			cw->players[i].exe_size);
+	}
+	return (FT_OK);
+}
+
 int		main(int argc, char **argv)
 {
 	t_corewar	cw;
-	
+	int			err;
+
+	err = FT_OK;
+	err = ft_process_args(&cw, argc, argv);
+	if (err == FT_OK)
+		err = ft_prepare_field(&cw);
+	//if (err == FT_OK)
+	//	err = ft_play(&cw);
+	//ft_output(&cw);
+	//ft_cw_free(&cw);
 	return (0);
 }
