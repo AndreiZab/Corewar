842ceedc20383138cea95650f5da5c0da188ddca
Leyla Arlyne <larlyne@e6r4p8.21-school.ru>
2019-07-24 16:21:16 +0300

    dump flag upd

--- src/cw/dump.c
+++ src/cw/dump.c
@@ -7,16 +7,16 @@ int ft_show_dump(t_corewar *cw)
 	char	*byte;
 
 	i = -1;
-	while (++i * FT_DUMP_ROW_SIZE <= MEM_SIZE)
+	while (++i * FT_DUMP_ROW_SIZE < MEM_SIZE)
 	{
+		ft_putstr("0x==== :");
 		j = 0;
 		while (j < FT_DUMP_ROW_SIZE && j + i * FT_DUMP_ROW_SIZE < MEM_SIZE)
 		{
 			if ((byte = casting_uc_hex(cw->map[j + i * FT_DUMP_ROW_SIZE]))
 					== NULL)
 				return (FT_MEMORY);
-			if (j != 0)
-				ft_putchar(' ');
+			ft_putchar(' ');
 			ft_putstr(byte);
 			free(byte);
 			++j;
